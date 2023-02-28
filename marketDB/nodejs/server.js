3afce1306eea2b71b9aebc36af48bfa400cd499d
vigneshwars089 <vign12320.it@rmkec.ac.in>
2023-02-28 13:03:04 -0600

    Create server.js

---
+++ marketDB/nodejs/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const chatApp = express();
+
+const http = require("http");
+
+const server = http.createServer(chatApp);
+
+const { Server } = require("socket.io");
+
+
+
+const io = new Server(server, {
+
+  cors: {
+
+    origin: "*",
+
+    methods: ["GET", "POST"],
+
+  },
+
+});
+
+
+
+io.on("connection",(socket)=>{
+
+    console.log('A user Connected');
+
+    socket.on("message",(msg)=>{
+
+        console.log(`user: ${msg.user} / message: ${msg.message}`);
+
+        socket.broadcast.emit("message",{user: msg.user, message: msg.message})
+
+
+
+    })
+
+
+
+})
+
+
+
+const port = 3001
+
+server.listen(port, ()=>{
+
+    console.log(` Started Server on port ${port}`);
+
+})
